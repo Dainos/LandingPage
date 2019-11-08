fcb3b63bb80bb8932048c5b467fc204d655f2753
Dainos <cauton777@gmail.com>
2019-11-08 17:57:23 +0200

    Added vendor prefixes

--- webpack.config.js
+++ webpack.config.js
@@ -8,42 +8,42 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
 
-//   module: {
-//     rules: [
-//         {
-//             test: /\.scss$/,
-//             use: [
-//                 {
-//                     loader: 'style-loader'
-//                 },
-//                 {
-//                     loader: 'css-loader',
-//                     options: {
-//                         sourceMap: true
-//                     }
-//                 },
-//                 {
-//                     loader: 'postcss-loader',
-//                     options: {
-//                         plugins: [
-//                             autoprefixer({
-//                                 browsers:['ie >= 8', 'last 4 version']
-//                             })
-//                         ],
-//                         sourceMap: true
-//                     }
-//                 },
-//                 {
-//                     loader: 'sass-loader',
-//                     options: {
-//                         includePaths: [
-//                             helpers.root('src', 'styles', 'global'),
-//                         ],
-//                         sourceMap: true
-//                     }
-//                 }
-//             ],
-//         },
-//     ]
-// }
+  module: {
+    rules: [
+        {
+            test: /\.css$/,
+            use: [
+                {
+                    loader: 'style-loader'
+                },
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: [
+                            autoprefixer({
+                                browsers:['Firefox <= 20', 'Firefox >= 20', 'last 4 version']
+                            })
+                        ],
+                        sourceMap: true
+                    }
+                },
+                // {
+                //     loader: 'sass-loader',
+                //     options: {
+                //         includePaths: [
+                //             helpers.root('src', 'styles', 'global'),
+                //         ],
+                //         sourceMap: true
+                //     }
+                // }
+            ],
+        },
+    ]
+}
 };
